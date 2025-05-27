3b0c8902663f18cd3da4c3bd135fa4b39c5e6bd0
abhishekpradeep <abhishek@keyvalue.systems>
2025-05-27 11:32:08 +0000

    css styling

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 def hello_world(): 
   return render_template('home.html')
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
